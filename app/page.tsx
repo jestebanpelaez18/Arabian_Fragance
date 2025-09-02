773db3bc9623a8b70f09ebe682d49fc031f9c191
Juan Esteban Pelaez Hoyos <juanes.pelaez18@gmail.com>
2025-09-02 19:36:54 +0300

    feat(home): scaffold hero section with background image and text content

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,27 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-       <h1>Hello, Home</h1>
+    <div>
+      <section className="relative min-h-screen">
+        <Image
+          className="object-cover object-center"
+          src="/hero/heroPicture.jpg"
+          alt="Arabian Fragance - Hero Picture"
+          fill={true}
+          priority
+          sizes="100vw"
+        />
+        <div className="absolute inset-0 z-10 flex items-center">
+          <div className="px-6 md:px-16 max-w-screen-xl w-full mx-auto flex flex-col items-start gap-4 text-white">
+            <h1 className="font-display text-5xl md:text-7xl leading-none tracking-tight">
+            DISCOVER THE<br />ESSENCE OF<br />ARABIAN<br />LUXURY
+            </h1>
+            <p className="font-serif text-base md:text-lg">
+            Experience exclusive fragrances from Dubai: bold, sensual, and <br />unforgettable.
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 }
