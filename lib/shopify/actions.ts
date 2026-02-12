f13f51498ace4c83ae112a68f9b86cfe9bda47c6
Juan Esteban Pelaez Hoyos <juanes.pelaez18@gmail.com>
2026-02-12 17:15:47 +0200

    feat: update product and search data types for improved type safety in Shopify actions

--- lib/shopify/actions.ts
+++ lib/shopify/actions.ts
@@ -2,6 +2,44 @@
 
 import { shopifyFetch } from "@/lib/shopify/shopify";
 
+// --- TYPES ---
+type Product = {
+  id: string;
+  title: string;
+  handle: string;
+  availableForSale: boolean;
+  priceRange: {
+    minVariantPrice: {
+      amount: string;
+      currencyCode: string;
+    };
+  };
+  featuredImage: {
+    url: string;
+    altText?: string;
+    width?: number;
+    height?: number;
+  };
+};
+
+// What we expect from Shopify's GraphQL API when fetching a single product
+
+type TrendingData = {
+  data: {
+    products: {
+      nodes: Product[];
+    };
+  };
+};
+
+type SearchData = {
+  data: {
+    search: {
+      nodes: Product[];
+    };
+  };
+};
+
 const trendingProductsQuery = `
   query trendingProducts{
     products(first: 10, sortKey: CREATED_AT, reverse: true) {
@@ -53,13 +91,11 @@ const searchProductsQuery = `
 
 export async function getTrendingProducts() {
   try {
-    const res = await shopifyFetch<any>({
+    const res = await shopifyFetch<TrendingData>({
       query: trendingProductsQuery,
       cache: "no-store",
     });
-
     const products = res.body?.data?.products?.nodes || [];
-
     const formattedProducts = products
       .sort(() => 0.5 - Math.random())
       .slice(0, 7);
@@ -74,7 +110,7 @@ export async function getTrendingProducts() {
 export async function searchProductsAction(term: string) {
   if (!term) return [];
 
-  const res = await shopifyFetch<any>({
+  const res = await shopifyFetch<SearchData>({
     query: searchProductsQuery,
     variables: {
       query: term,
